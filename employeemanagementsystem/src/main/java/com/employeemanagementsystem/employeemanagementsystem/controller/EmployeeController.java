434df002cec49dc734beb545436cd5527a7c976f
Rahul <rahulsamir0@gmail.com>
2023-12-23 12:53:46 +0530

    Create Employee REST API Implemented

--- employeemanagementsystem/src/main/java/com/employeemanagementsystem/employeemanagementsystem/controller/EmployeeController.java
+++ employeemanagementsystem/src/main/java/com/employeemanagementsystem/employeemanagementsystem/controller/EmployeeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.employeemanagementsystem.employeemanagementsystem.model.Employee;
 import com.employeemanagementsystem.employeemanagementsystem.repository.EmployeeRepository;
@@ -18,9 +20,17 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	
+	//get all employees
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();		
+	}
+	
+	//create employee rest api
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 	
 	
